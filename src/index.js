7c7ddf86360cc1fddae0ae642f75ba662c0e500c
Ramya Iyer <ramyaiyer1000@gmail.com>
2025-10-10 02:58:53 -0700

    fixed redirect on website

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+const redirectParam = new URLSearchParams(window.location.search).get("redirect");
+if (redirectParam) {
+  window.history.replaceState(null, "", decodeURIComponent(redirectParam));
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter basename="/">
