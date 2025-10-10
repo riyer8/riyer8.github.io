be61af58938f875840186c2fbb5ecb385d1da647
Ramya Iyer <ramyaiyer1000@gmail.com>
2025-10-10 02:40:11 -0700

    updating /bookshelf

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import React from 'react';
-import { ThemeProvider } from './context/ThemeContext';
-import { PixelatedBackground, Sidebar, MainContent, ThemeToggle } from './components';
-import BookshelfPage from './pages/BookshelfPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { PixelatedBackground, Sidebar, MainContent, ThemeToggle } from "./components";
+import BookshelfPage from "./pages/BookshelfPage";
 
 const App = () => {
-    const containerStyle = {
-        position: 'relative',
-        zIndex: 1,
-        minHeight: '100vh',
-        display: 'flex',
-        fontFamily: '"Inter", -apple-system, BlinkMacSystemFont, sans-serif',
-        margin: 0,
-        padding: 0
-    };
+  const containerStyle = {
+    position: "relative",
+    zIndex: 1,
+    minHeight: "100vh",
+    display: "flex",
+    fontFamily: '"Inter", -apple-system, BlinkMacSystemFont, sans-serif',
+    margin: 0,
+    padding: 0,
+  };
 
-    React.useEffect(() => {
-        document.body.style.margin = '0';
-        document.body.style.padding = '0';
-        document.body.style.overflowX = 'hidden';
-        // Remove the static backgroundColor since it will be handled by the theme
-    }, []);
+  React.useEffect(() => {
+    document.body.style.margin = "0";
+    document.body.style.padding = "0";
+    document.body.style.overflowX = "hidden";
+  }, []);
 
-    const isBookshelf = typeof window !== 'undefined' && window.location && window.location.pathname === '/bookshelf';
-
-    return (
-        <ThemeProvider>
-            <PixelatedBackground theme="whoami" />
-            <ThemeToggle />
-            {isBookshelf ? (
-                <BookshelfPage />
-            ) : (
-                <div style={containerStyle}>
-                    <Sidebar />
-                    <MainContent />
-                </div>
-            )}
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider>
+      <PixelatedBackground theme="whoami" />
+      <ThemeToggle />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div style={containerStyle}>
+              <Sidebar />
+              <MainContent />
+            </div>
+          }
+        />
+        <Route path="/bookshelf" element={<BookshelfPage />} />
+      </Routes>
+    </ThemeProvider>
+  );
 };
 
 export default App;
